f6f4cafeaf5a4dfe5748ba649f5c4371567b83df
joshiabastian <j.bastian21197@gmail.com>
2026-01-16 10:40:24 +0700

    commit: update etl scripts

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from config.setting import PRODUCT_PATH, REQRUITMENT_PATH
-from scripts.extract import extract_product, extract_reqrutment
+from scripts.extract import extract_product, extract_reqruitment
 
-# product_df = extract_product(PRODUCT_PATH)
-# print(product_df)
+product_df = extract_product(PRODUCT_PATH)
+print(product_df)
 
-reqruitment_df = extract_reqrutment(REQRUITMENT_PATH)
-print(reqruitment_df)
+# reqruitment_df = extract_reqruitment(REQRUITMENT_PATH)
+# print(reqruitment_df)
--- scripts/extract.py
+++ scripts/extract.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from glob import glob
 
+# Extract data product
 
 def extract_product(folder_path):
     files = glob(f"{folder_path}/*.csv")
     dfs = [pd.read_csv(file) for file in files]
     return pd.concat(dfs, ignore_index=True)
 
+# Extract data reqruitment
 
-def extract_reqrutment(file_path):
+def extract_reqruitment(file_path):
     return pd.read_csv(file_path)
