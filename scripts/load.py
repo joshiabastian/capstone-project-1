ab9a08c5b7adc463d4604e992c2322e6b85eaefd
joshiabastian <j.bastian21197@gmail.com>
2026-01-20 00:37:15 +0700

    commit: Update ETL scripts

--- scripts/load.py
+++ scripts/load.py
@@ -11,4 +11,4 @@ def load_data(df, folder_path=None, file_name=None):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     df.to_csv(os.path.join(folder_path, file_name), index=False)
-    print(f"Data berhasil di load!")
+    print(f"Data berhasil di load!!")
